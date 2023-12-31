9e587e30c9fbd38c691483c2fb6a758f1b6638ad
CodingManoj <CodingManoj@gmail.com>
2023-12-31 06:57:41 +0530

    Provisioning VPC

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-module "vpc" {
-    source  = "git::https://github.com/b56-clouddevops/tf-module-vpc.git"
 
-    VPC_CIDR = var.VPC_CIDR
+module "vpc" {
+  source = "git::https://github.com/b56-clouddevops/tf-module-vpc.git"
+  VPC_CIDR = var.VPC_CIDR
 }
