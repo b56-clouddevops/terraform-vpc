93e204fbbd0d84f0b28a231107552b3e5fe0774f
CodingManoj <CodingManoj@gmail.com>
2024-01-03 07:14:54 +0530

    TF VPC Auto Push

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ module "vpc" {
   source              = "./vendor/modules/vpc"
   
   VPC_CIDR            = var.VPC_CIDR
+  AZ                  = var.AZ
   ENV                 = var.ENV
   PUBLIC_SUBNET_CIDR  = var.PUBLIC_SUBNET_CIDR
   PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
