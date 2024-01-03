98383c80bef401835b66804996ea5e46bd2178d4
CodingManoj <CodingManoj@gmail.com>
2024-01-03 06:55:56 +0530

    TF VPC Auto Push

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source              = "git::https://github.com/b56-clouddevops/tf-module-vpc.git"
   
   VPC_CIDR            = var.VPC_CIDR
-  # ENV           = var.ENV
+  # ENV               = var.ENV
   PUBLIC_SUBNET_CIDR  = var.PUBLIC_SUBNET_CIDR
   PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
 }
