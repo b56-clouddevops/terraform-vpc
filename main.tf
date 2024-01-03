9df93c78dbbf203e9357797060af5802ae022504
CodingManoj <CodingManoj@gmail.com>
2024-01-03 06:21:03 +0530

    TF VPC Auto Push

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source        = "git::https://github.com/b56-clouddevops/tf-module-vpc.git"
   
-  env           = var.env
+  # env           = var.env
   VPC_CIDR      = var.VPC_CIDR
 }
