f864cd06fcc7c5db6038a13e980afe6ee1eb479e
CodingManoj <CodingManoj@gmail.com>
2024-01-10 06:49:11 +0530

    TF DB Auto Push

--- main.tf
+++ main.tf
@@ -1,15 +1,20 @@
 module "vpc" {
   # source              = "git::https://github.com/b56-clouddevops/tf-module-vpc.git"
-  source              = "./vendor/modules/vpc"
+  source                    = "./vendor/modules/vpc"
   
-  VPC_CIDR            = var.VPC_CIDR
-  AZ                  = var.AZ
-  ENV                 = var.ENV
-  PUBLIC_SUBNET_CIDR  = var.PUBLIC_SUBNET_CIDR
-  PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
-  DEFAULT_VPC_CIDR    = var.DEFAULT_VPC_CIDR
-  DEFAULT_VPC_ID      = var.DEFAULT_VPC_ID
-  DEFAULT_VPC_RT      = var.DEFAULT_VPC_RT
+  VPC_CIDR                  = var.VPC_CIDR
+  AZ                        = var.AZ
+  ENV                       = var.ENV
+  PUBLIC_SUBNET_CIDR        = var.PUBLIC_SUBNET_CIDR
+  PRIVATE_SUBNET_CIDR       = var.PRIVATE_SUBNET_CIDR
+  DEFAULT_VPC_CIDR          = var.DEFAULT_VPC_CIDR
+  DEFAULT_VPC_ID            = var.DEFAULT_VPC_ID
+  DEFAULT_VPC_RT            = var.DEFAULT_VPC_RT
+  PUBLIC_HOSTED_ZONE_ID     = var.PUBLIC_HOSTED_ZONE_ID
+  PUBLIC_HOSTED_ZONE_NAME   = var.PUBLIC_HOSTED_ZONE_NAME
+
+  PRIVATE_HOSTED_ZONE_ID    = var.PRIVATE_HOSTED_ZONE_ID
+  PRIVATE_HOSTED_ZONE_NAME  = var.PRIVATE_HOSTED_ZONE_NAME
 }
 
 
