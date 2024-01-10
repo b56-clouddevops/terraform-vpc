dee531b716772f07b92bba6dbd112be5684bbecc
CodingManoj <CodingManoj@gmail.com>
2024-01-10 06:48:05 +0530

    TF DB Auto Push

--- outputs.tf
+++ outputs.tf
@@ -29,3 +29,17 @@ output "DEFAULT_VPC_CIDR" {
 output "DEFAULT_VPC_ID" {
     value   = module.vpc.DEFAULT_VPC_ID
 }
+
+output "PUBLIC_HOSTED_ZONE_ID" {
+    value = var.PUBLIC_HOSTED_ZONE_ID
+}
+output "PUBLIC_HOSTED_ZONE_NAME" {
+    value = var.PUBLIC_HOSTED_ZONE_NAME
+}
+
+output "PRIVATE_HOSTED_ZONE_ID" {
+    value = var.PRIVATE_HOSTED_ZONE_ID
+}
+output "PRIVATE_HOSTED_ZONE_NAME" {
+    value = var.PRIVATE_HOSTED_ZONE_NAME
+}
--- terraform.auto.tfvars
+++ terraform.auto.tfvars
@@ -3,3 +3,9 @@ AZ                      = ["us-east-1a", "us-east-1b"]
 DEFAULT_VPC_CIDR        = "172.31.0.0/16"
 DEFAULT_VPC_ID          = "vpc-0031cc952da0c7bfc"
 DEFAULT_VPC_RT          = "rtb-0623fe36206b96d65"
+
+PUBLIC_HOSTED_ZONE_ID   = "Z03169043MR7GRPXZ59K1"
+PUBLIC_HOSTED_ZONE_NAME = "b56-roboshop.online"
+
+PRIVATE_HOSTED_ZONE_ID   = "Z031297333JO38PNHPROR"
+PRIVATE_HOSTED_ZONE_NAME = "roboshop.internal"
